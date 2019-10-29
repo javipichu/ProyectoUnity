0d8f8c1467c506c368209d00b78ddb6fed1acd00
javi DAM 1 <43582936+javipichu@users.noreply.github.com>
2019-10-29 19:40:18 +0100

    Add files via upload

---
+++ scrips/PlayerController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class PlayerController : MonoBehaviour {
+
+private Rigidbody rb;
+private int count;
+public float speed;
+public Text countText;
+public Text winText;
+	void SetCountText(){
+		countText.text="Score : " + count.ToString();
+		if (count >= 13) {
+ 		winText.text = "CRACK !!";
+		 
+}
+	}
+
+	void Start () {
+		rb = GetComponent<Rigidbody>();
+		count=0;
+		SetCountText();
+		winText.text = "";
+
+	}
+	
+	void Update () {
+		 float moveHorizontal = Input.GetAxis("Horizontal");
+ 		float moveVertical = Input.GetAxis("Vertical");
+ 		Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+ 		rb.AddForce(movement * speed);
+	}
+	void OnTriggerEnter(Collider other) {
+ 		if (other.gameObject.tag == "PickUp") {
+ 		other.gameObject.SetActive(false);
+ 		count = count +1;
+ 		SetCountText(); 
+ 		}
+		if (other.gameObject.tag =="caida"){
+			winText.text="GAME OVER!";
+			rb.gameObject.SetActive(false);
+		}
+
+}
+}
